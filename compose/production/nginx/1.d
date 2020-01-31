71d8c79dcd31297c157bcdae364de1d666fa3e95
Volin <j_yuefeng@163.com>
2020-01-31 13:51:49 +0800

    修改代码，docker部署

--- compose/production/nginx/1.d
+++ compose/production/nginx/1.d
@@ -1,12 +1,12 @@
-upstream hellodjango_blog_tutorial  {
-    server hellodjango_blog_tutorial:8000;
+upstream janmeery  {
+    server janmeery:8000;
 }
  
 server {
     server_name  janmeery.cn;
  
     location /static {
-        alias /apps/hellodjango_blog_tutorial/static;
+        alias /apps/janmeery/static;
     }
  
     location / {
@@ -15,7 +15,7 @@ server {
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
  
-        proxy_pass http://hellodjango_blog_tutorial;
+        proxy_pass http://janmeery;
     }
  
     listen 80;
